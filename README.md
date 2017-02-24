22625c673122f1151e572ed4b5b37beb59ccb796
d5c5ceb0 <d5c5ceb0@gmail.com>
2017-02-24 20:26:47 +0800

    add sm3 & sm4 algorithms

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sm_crypto
-golang implements for Chinese SM2/SM3/SM4 algorithms.
+# sm_crypto_golang
+golang implements for Chinese SM2/SM3/SM4 algorithms. 
+
+
+1. add sm_crypto_golang to $GOPATH/src/
+2. open sm3/, and use the command "go test" to test the SM3.
+3. open sm4/, and use the command "go test" to test the SM4.
---
+++ sm4/cipher.go
@@ -0,0 +1,42 @@
+/*
+ * author: weizhang <d5c5ceb0@gmail.com>
+ * 2017.02.24
+ */
+
+package sm4
+
+import (
+	"crypto/cipher"
+	"strconv"
+)
+
+// The SM4 block size in bytes.
+const BlockSize = 16
+
+type KeySizeError int
+
+func (k KeySizeError) Error() string {
+	return "sm4: invalid key size " + strconv.Itoa(int(k))
+}
+
+// sm4Cipher is an instance of SM4 encryption.
+type sm4Cipher struct {
+	subkeys [32]uint32
+}
+
+// NewCipher creates and returns a new cipher.Block.
+func NewCipher(key []byte) (cipher.Block, error) {
+	if len(key) != 16 {
+		return nil, KeySizeError(len(key))
+	}
+
+	c := new(sm4Cipher)
+	c.generateSubkeys(key)
+	return c, nil
+}
+
+func (c *sm4Cipher) BlockSize() int { return BlockSize }
+
+func (c *sm4Cipher) Encrypt(dst, src []byte) { encryptBlock(c.subkeys[:], dst, src) }
+
+func (c *sm4Cipher) Decrypt(dst, src []byte) { decryptBlock(c.subkeys[:], dst, src) }
